7fc72c7ef40598f88177255eda680a93bba0bc09
Kyle Deato <kyledeato@gmail.com>
2022-06-08 16:42:43 -0700

    updated projects and bio

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const moreProjects = () => {
+    let x = document.getElementById("projects2")
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        
+       
+      } else {
+        x.style.display = "none";
+        
+      }
+}
